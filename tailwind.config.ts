9143f5e1fb385dc9c84596d1fcd6377766e9da5b
rhaielyyce <rhaielyce@gmail.com>
2023-11-28 12:49:34 -0300

    Primeiro commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,13 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
+    extend: { 
+      colors: {
+      'pink-pink': '#B70084',
+    },
+
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+       background: "url('/bg.png')",
       },
     },
   },
